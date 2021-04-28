9e716bc9f0c90c80e60e7bdefb0f292b216d2426
dewei84 <83356605+dewei84@users.noreply.github.com>
2021-04-28 22:56:29 +0800

    Update ExpenseItem.js
    course progress

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
-import './ExpenseItem.css';
+import "./ExpenseItems.css";
 
 function ExpenseItem() {
-    return (
-      <div className="expense-item"> 
-        <div>March 28th 2021</div>
-        <div className="expense-item__description">
-          <h2>Car Insurance</h2>
-          <div className="expense-item__price">$294.67</div>
-        </div>
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 588;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
-    );
-  }
-  
-  export default ExpenseItem;
+    </div>
+  );
+}
+
+export default ExpenseItem;
